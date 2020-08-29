a6dba7f19e5151886f44cbd5e4bb14c92cd69d5f
lamouu <69391156+lamouu@users.noreply.github.com>
2020-08-29 21:53:59 +0100

    Added Piece Values

---
+++ js/board.js
@@ -0,0 +1,10 @@
+var GameBoard {};
+
+GameBoard.pieces = new Array(BRD_SQ_NUM);
+GameBoard.side = COLOURS.WHITE;
+GameBoard.fiftyMove = 0;
+GameBoard.hisPly = 0;
+GameBoard.Ply = 0;
+GameBoard.castlePerm = 0;
+GameBoard.material = new Array(2);
+GameBoard.pceNum = new Array(13);
